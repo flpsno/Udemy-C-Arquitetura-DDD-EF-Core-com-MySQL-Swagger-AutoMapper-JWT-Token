a320aedefe061e7c536b72fb4c29e853522d6fd1
Felipe Santos <flpsno@hotmail.com>
2020-04-10 11:21:16 -0300

    Adicinado IRepository.

---
+++ src/Api.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+
+namespace Api.Domain.Interfaces
+{
+    public interface IRepository<T> where T: BaseEntity
+    {
+         Task<T> InsertAsync(T entity);
+         Task<T> UpdateAsync(T entity);
+         Task<bool> DeleteAsync(Guid id);
+         Task<T> SelectAsync(int id);
+         Task<IEnumerable<T>> SelectAsync();
+    }
+}
